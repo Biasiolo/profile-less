2d2ac0076db7a97a03ac3686657c71c5408383b7
Biasiolo <biasiolorafael@gmail.com>
2024-02-21 10:54:18 -0300

    ajustes

--- src/script.js
+++ src/script.js
@@ -8,3 +8,15 @@ function openModal(url) {
     document.getElementById('modal').style.display = 'none';
   }
   
+  // Abre o modal
+function abrirModal() {
+  document.getElementById('projetoModal').style.display = 'block';
+}
+
+// Fecha o modal
+function fecharModal() {
+  document.getElementById('projetoModal').style.display = 'none';
+}
+
+// Adiciona evento de clique ao botão para abrir o modal
+document.getElementById('abrirModalBtn').addEventListener('click', abrirModal);
